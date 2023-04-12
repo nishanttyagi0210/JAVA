1bb5b25d90f5256a2d6c23d1dd267b997e7933f1
Nishant Tyagi <121551477+nishanttyagi0210@users.noreply.github.com>
2023-04-12 14:58:02 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,18 @@
+package nishant;
+
+public class demo {
+	
+	protected void show()
+	   {
+	  System.out.println("Yes i am in main in package nishant and class demo");
+	   }
+	
+	public static void main(String[] args)
+	{
+
+     demo s1 = new demo();
+    s1.show();
+		
+	}
+
+}
