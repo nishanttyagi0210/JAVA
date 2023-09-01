587356b33f466f743bc8af0337a9f495df76a26a
Nishant Tyagi <121551477+nishanttyagi0210@users.noreply.github.com>
2023-09-01 19:36:38 +0530

    Add files via upload

---
+++ parse.java
@@ -0,0 +1,13 @@
+public class parse {
+
+    public static void main(String[] args) {
+
+        String a = "5";
+        String b = "5";
+
+        int num = Integer.valueOf(a);
+        int num1 = Integer.valueOf(b);
+        System.out.println("sum after parsed that of " + num + "and  " + num1 + "is " + (num + num1));
+    }
+
+}
