4cd3a83d42cef2a2c0babf1ea0b381b709437159
Nishant Tyagi <121551477+nishanttyagi0210@users.noreply.github.com>
2023-09-01 19:35:22 +0530

    Add files via upload

---
+++ SumOfElementsArray.java
@@ -0,0 +1,16 @@
+class SumOfElementsArray {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int i;
+        int sum = 0;
+
+        int[] arr = { 1, 2, 3, 4, 5 };
+        for (i = 0; i < 5; i++) {
+            // if (arr[i] = arr[]) {
+            // System.out.println("yes");
+            sum = arr[i] + sum;
+        }
+        System.out.println(sum);
+    }
+
+}
