4b934315226f2c98742942a83c14e39b30e645b7
Nishant Tyagi <121551477+nishanttyagi0210@users.noreply.github.com>
2023-11-27 16:10:26 +0530

    Add files via upload

---
+++ placement_p.java
@@ -0,0 +1,30 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int a[]=new int[5];
+        String name[]=new String[21];
+        //declaration and instantiation  
+    a[0]=90;
+    a[1]=80;  
+    a[2]=70;  
+    a[3]=40;  
+    a[4]=50;
+    
+     name[0]="Sushant";
+    name[1]="Nishant";  
+    name[2]="Negi";
+     name[3]="Prashant";
+      name[4]="Ronak";
+    
+    
+    for(int i=0;i<5;i++){
+ System.out.println("\n" +name[i]);
+    System.out.println(a[i]);
+     
+}
+    }
+    
+}
